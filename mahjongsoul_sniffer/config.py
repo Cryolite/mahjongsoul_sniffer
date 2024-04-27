869d4e18b6065cc8dec61f891f6ba51a8f2af7f6
Apricot-S <mahjong_apricot@outlook.jp>
2024-04-27 10:38:44 +0000

    refactor: Combine function definitions into one file

--- mahjongsoul_sniffer/config.py
+++ mahjongsoul_sniffer/config.py
@@ -1,14 +1,29 @@
 import importlib
+import pathlib
 from functools import cache
 
-from mahjongsoul_sniffer.config_loader import load_config
+import jsonschema
+import yaml
+
+
+def _load(file_path: pathlib.Path, schema: dict[str, object]) -> dict:
+    if not file_path.exists():
+        msg = f"{file_path}: File does not exist."
+        raise RuntimeError(msg)
+    if not file_path.is_file():
+        msg = f"{file_path}: Not a file."
+        raise RuntimeError(msg)
+
+    with file_path.open() as config_file:
+        config = yaml.load(config_file, Loader=yaml.Loader)
+
+    jsonschema.validate(instance=config, schema=schema)
+
+    return config
 
 
 @cache
 def get(module_name: str) -> dict:
     config_module_path = f"mahjongsoul_sniffer.{module_name}.config"
     config_module = importlib.import_module(config_module_path)
-    return load_config(
-        config_module.CONFIG_FILE_PATH,
-        config_module.CONFIG_SCHEMA,
-    )
+    return _load(config_module.CONFIG_FILE_PATH, config_module.CONFIG_SCHEMA)
